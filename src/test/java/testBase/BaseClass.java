9d37e3238029dc332a4c05d1cb352073a4660d84
Sangeeth K S <sangeethks913@gmail.com>
2026-05-23 15:26:54 +0530

    selenium grid

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -12,10 +12,14 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.net.URL;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -25,68 +29,96 @@ public class BaseClass {
 	public Logger logger;
 	public Properties p;
 
-	@BeforeClass(groups= {"Sanity","Regression","Master"})
-	@Parameters({"os","browser"})
+	@BeforeClass(groups = { "Sanity", "Regression", "Master" })
+	@Parameters({ "os", "browser" })
 	public void setup(String os, String br) throws IOException {
 
-	    // Load config.properties from classpath
-	    InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties");
-	    p = new Properties();   // <-- use the class field, not a new local variable
-	    p.load(input);
+		// Load config.properties from classpath
+		InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties");
+		p = new Properties(); // <-- use the class field, not a new local variable
+		p.load(input);
 
-	    logger = LogManager.getLogger(this.getClass());
+		logger = LogManager.getLogger(this.getClass());
 
-	    switch (br.toLowerCase()) {
-	        case "chrome":
-	            driver = new ChromeDriver();
-	            break;
-	        case "edge":
-	            driver = new EdgeDriver();
-	            break;
-	        default:
-	            System.out.println("Invalid Browser Name");
-	            return;
-	    }
+		if (p.getProperty("execution_env").equalsIgnoreCase("remote")) {
+			DesiredCapabilities capabilities = new DesiredCapabilities();
 
-	    driver.manage().window().maximize();
+			// os
+			if (os.equalsIgnoreCase("windows")) {
+				capabilities.setPlatform(Platform.WIN11);
+			} else {
+				System.out.println("No matching os");
+				return;
+			}
+
+			// browser
+			switch (br.toLowerCase()) {
+			case "chrome":
+				capabilities.setBrowserName("chrome");
+				break;
+			case "edge":
+				capabilities.setBrowserName("edge");
+				break;
+			default:
+				System.out.println("Invalid Browser Name");
+				return;
+			}
+			
+			driver=new RemoteWebDriver(new URL(p.getProperty("gridUrl")),capabilities);
+
+		}
+
+		if (p.getProperty("execution_env").equalsIgnoreCase("local")) {
+			switch (br.toLowerCase()) {
+			case "chrome":
+				driver = new ChromeDriver();
+				break;
+			case "edge":
+				driver = new EdgeDriver();
+				break;
+			default:
+				System.out.println("Invalid Browser Name");
+				return;
+			}
+		}
+		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-	    driver.get(p.getProperty("appUrl"));
+		driver.get(p.getProperty("appUrl"));
 	}
-	
-	
-	@AfterClass(groups= {"Sanity","Regression","Master"})
+
+	@AfterClass(groups = { "Sanity", "Regression", "Master" })
 	public void tearDown() {
-		if(driver!=null) {
+		if (driver != null) {
 			driver.quit();
 		}
 	}
-	
+
 	public String randomString() {
 		return RandomStringUtils.randomAlphabetic(5);
 	}
-	
+
 	public String randomNumber() {
 		return RandomStringUtils.randomNumeric(10);
 	}
-	
+
 	public String randomAlphaNumeric() {
 		return RandomStringUtils.randomAlphabetic(3) + RandomStringUtils.randomNumeric(3);
 	}
-	
+
 	public String captureScreen(String tname) throws IOException {
 
 		String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-				
+
 		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
 		File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
-		
-		String targetFilePath=System.getProperty("user.dir")+"\\screenshots\\" + tname + "_" + timeStamp + ".png";
-		File targetFile=new File(targetFilePath);
-		
+
+		String targetFilePath = System.getProperty("user.dir") + "\\screenshots\\" + tname + "_" + timeStamp + ".png";
+		File targetFile = new File(targetFilePath);
+
 		sourceFile.renameTo(targetFile);
-			
+
 		return targetFilePath;
 
 	}
